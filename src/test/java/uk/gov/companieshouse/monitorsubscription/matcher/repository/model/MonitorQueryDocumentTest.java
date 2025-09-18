75c69b4571985f78ff106083104284ee1d11ec68
jbishop2-ch <jbishop2@companieshouse.gov.uk>
2025-09-18 10:45:00 +0100

    Conatinerised tests for Mongo DB

--- src/test/java/uk/gov/companieshouse/monitorsubscription/matcher/repository/model/MonitorQueryDocumentTest.java
+++ src/test/java/uk/gov/companieshouse/monitorsubscription/matcher/repository/model/MonitorQueryDocumentTest.java
@@ -2,8 +2,14 @@ package uk.gov.companieshouse.monitorsubscription.matcher.repository.model;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.ACTIVE;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.COMPANY_NUMBER;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.CREATED_DATE;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.ID;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.QUERY;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.UPDATED_DATE;
+import static uk.gov.companieshouse.monitorsubscription.matcher.util.TestUtils.USER_ID;
 
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,14 +17,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class MonitorQueryDocumentTest {
-
-    private static final String ID = "654321";
-    private static final String COMPANY_NUMBER = "00006400";
-    private static final LocalDateTime CREATED_DATE = LocalDateTime.parse("2023-10-10T10:00:00");
-    private static final Boolean ACTIVE = Boolean.TRUE;
-    private static final LocalDateTime UPDATED_DATE = CREATED_DATE.plusDays(1);
-    private static final String QUERY = "QUERY transaction WHERE company_number=\"%s\"".formatted(COMPANY_NUMBER);
-    private static final String USER_ID = "";
 
     MonitorQueryDocument underTest;
 
