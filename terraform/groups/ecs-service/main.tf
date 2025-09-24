8a1ceb0ac98b479b6ecd398e22fecf7570e076d8
Mike Kelly <mkelly1@companieshouse.gov.uk>
2025-09-24 15:23:24 +0100

    Update terraform.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -54,7 +54,7 @@ module "ecs-service" {
   # Docker container details
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
-  container_version = var.chs_monitor_subscription_matcher_version
+  container_version = var.monitor_subscription_matcher_version
   container_port    = local.container_port
 
   # Service configuration
